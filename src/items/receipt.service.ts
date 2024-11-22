a1aa1e87358d95e67ef9836b19ad7655fb14a17d
Christoph Larson <47951382+ChristophLarson@users.noreply.github.com>
2024-11-22 17:56:10 -0600

    init

---
+++ src/items/receipt.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
+
+@Injectable()
+export class ItemsService {
+  process(receipt:)
+}
