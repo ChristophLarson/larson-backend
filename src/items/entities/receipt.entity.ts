c531ba5e7d3665a2d94564f6f4c79d872b546424
Christoph Larson <47951382+ChristophLarson@users.noreply.github.com>
2024-11-22 18:18:39 -0600

    set up sqlite

--- src/items/entities/receipt.entity.ts
+++ src/items/entities/receipt.entity.ts
@@ -1,58 +1,20 @@
 
-export class ReceiptItem {
-    /**
-     * Description of purchased item.
-     */
-    shortDescription: string;
-  
-    /**
-     * Price of the item as a string.
-     */
-    price: string;
-  
-    constructor(shortDescription: string, price: string) {
-      this.shortDescription = shortDescription.trim();
-      this.price = price;
-    }
-  }
-  export class Receipt {
-    /**
-     * The retailer
-     */
-    retailer: string;
-  
-    /**
-     * Date of the purchase in YYYY-MM-DD format
-     */
-    purchaseDate: string;
-  
-    /**
-     * Time of the purchase in HH:mm format.
-     */
-    purchaseTime: string;
-  
-    /**
-     * List of purchased items.
-     */
-    items: ReceiptItem[];
-  
-    /**
-     * Total amount of the purchase as a string.
-     */
-    total: string;
-  
-    constructor(
-      retailer: string,
-      purchaseDate: string,
-      purchaseTime: string,
-      items: ReceiptItem[],
-      total: string
-    ) {
-      this.retailer = retailer;
-      this.purchaseDate = purchaseDate;
-      this.purchaseTime = purchaseTime;
-      this.items = items;
-      this.total = total;
-    }
-  }
-  
+import { Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class Receipt {
+  @Column()
+  id: string;
+
+  @Column()
+  retailer: string;
+
+  @Column()
+  purchaseDate: string;
+
+  @Column()
+  purchaseTime: string;
+
+  @Column()
+  total: string;
+}
--- src/items/receipt.service.ts
+++ src/items/receipt.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Receipt } from './entities/receipt.entity';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 
 @Injectable()
 export class ItemsService {
-  process(receipt:)
+  process(receipt: Receipt): string {
+    return "test";
+  }
 }
