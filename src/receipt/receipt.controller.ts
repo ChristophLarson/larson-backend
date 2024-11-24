966c43f092f7de6f8583555ef7f07707c1b62db9
Christoph Larson <47951382+ChristophLarson@users.noreply.github.com>
2024-11-24 12:59:44 -0600

    Add endpoints to controller

--- src/receipt/receipt.controller.ts
+++ src/receipt/receipt.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ReceiptService } from './receipt.service';
 
 @Controller('receipt')
 export class ReceiptController {
+  private receipt: Record<string, any> = {};
   constructor(private readonly receiptService: ReceiptService) {}
 
-  @Get()
-  findAll() {
-    //return this.receiptService.findAll();
+  @Post('/process')
+  async processReceipt(@Body() receipt: any) {
+    const id = await this.receiptService.processReceipt(receipt);
+    return { id };
+  }
+
+  @Get('/:id/points')
+  async getPoints(@Param('id') id: string): Promise<{points: number}> {
+    try {
+      const points = await this.receiptService.calculatePoints(id);
+      return { points };
+    } catch (error) {
+      throw new Error(`Error calculating points: ${error}`);
+    }
   }
 }
